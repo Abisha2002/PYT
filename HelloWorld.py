b3692295677be18cf544ae54693f875a76657b81
abisha <33662@ma-web.nl>
2021-10-07 09:45:13 +0200

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+print("Hello you!, ik ben Abisha")
+name = input("Wie ben jij? ")
+print("hello " + name ) 
+
+import datetime
+x = datetime.datetime.now()
+
+print(x)
+
+print("Ik ben een nieuwkomer op het Mediacollege Amsterdam door een aantal vragen over mij te beantwoorden leer je mij beter kennen")
