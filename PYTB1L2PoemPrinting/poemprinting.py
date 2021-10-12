af1a73d1d44003a816b00805136f7093c288feb3
abisha <33662@ma-web.nl>
2021-10-12 12:10:41 +0200

    het bestand verplaatst naar de juiste map

---
+++ PYTB1L2PoemPrinting/poemprinting.py
@@ -0,0 +1,10 @@
+vers1 = "Mijn naam is Abisha Annalingam"
+vers2 = "Ik vind het heel druk in Amsterdam"
+vers3 = "Den Helder daar kom ik vandaan"
+vers4 = "Naast schooldagen ben ik op mijn bijbaan"
+Vers5 = "Mijn bijbaan is vakkenvullen"
+vers6 = "Dat is zakkenvullen!"
+vers7 = "Dit was mijn gedicht"
+vers8 = "En het was volledig op mij gericht"
+
+print(vers1, vers2, vers3, vers4, Vers5, vers6, vers7, vers8 )
